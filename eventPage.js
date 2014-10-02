77803bde737b8b54b2e5f07264c9b47c37400ad8
Tobi Weinstock <tv.weinstock@gmail.com>
2014-10-02 18:24:59 -0400

    first commit

---
+++ eventPage.js
@@ -0,0 +1,19 @@
+
+
+
+// Use ttcStations() to load the stations
+// use ngRepeat to list the stations on the page
+
+// Place filter on station results
+// Create a searcher input field
+
+// Figure out what station has been selected
+// show time of next departure
+
+
+
+
+
+
+// Geolocation 
+// ttcNearby()
