408f286c1b6104732e1ddefbbe307b861242110f
Tobi Weinstock <tv.weinstock@gmail.com>
2014-10-02 23:01:46 -0400

    bus route name appended to html via jquery

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,5 @@
 var ttc = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=32"
-var ttc = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=32"
+
 
 function get_station(station_number) {
   return "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=" + station_number
@@ -8,29 +8,20 @@ function get_station(station_number) {
 var json = {};
 
 
-
 $.get(ttc, function(xml){
   json = $.xml2json(xml);
+  $('#route').html((json.route.title));
+
+
+  function findAllStops() {
+    var stops = json.route.stop
+    console.log(stops)
+    for (var i=0; i<stops.length; i++) {
+      return(i);
+    };
+  }
+
 });
 
 
 
-
-
-
-// Use ttcStations() to load the stations
-// use ngRepeat to list the stations on the page
-
-// Place filter on station results
-// Create a searcher input field
-
-// Figure out what station has been selected
-// show time of next departure
-
-
-
-
-
-
-// Geolocation 
-// ttcNearby()
