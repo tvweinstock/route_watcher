e3dfdb5bda3a5759ad6a107cc491b877ed243df7
Tobi Weinstock <tv.weinstock@gmail.com>
2014-10-03 15:37:41 -0400

    station ids appearing as option values

--- js/eventPage.js
+++ js/eventPage.js
@@ -1,33 +1,38 @@
 var ttc = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=32"
-
-
-function get_station(station_number) {
-  return "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=" + station_number
-}
+var prediction = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId="
+// function not being called yet
+// function get_station(station_number) {
+//   return "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=" + station_number
+// }
 
 var json = {};
 
 
-$.get(ttc, function(xml){
+$.get(ttc, prediction, function(xml){
   json = $.xml2json(xml);
   $('#route').html((json.route.title));
 
 
-  
+  // stores names of stops
   var stops = json.route.stop
+  // iterates over each stop and collects stopId
   stops.forEach(function(stop, index){
-    // console.log("Stop #" + index + ": " + stop.title);
-    (stop.title).add($("<li></li>"));
+    $('#myTransit').append("<option value='" + stop.stopId + "'>" + stop.title + "</option>");
+
+  // use stopId that user selected to show predicted times  
+  $('myTransit').val(function(value) {
+    
 
   });
-    
+
+});
 });
 
 
-// convert XML to JSON
-// iterate over stops
-// select a stop
-// find upcoming bus times
+
+
+
+
 
 
 
