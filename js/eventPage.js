fa1a3df3c90ae8b74d0394e3fa3e784ad4b7ccad
Tobi Weinstock <tv.weinstock@gmail.com>
2014-10-07 23:18:49 -0400

    user can make multiple calls to the api in a single document.ready

--- js/eventPage.js
+++ js/eventPage.js
@@ -1,24 +1,18 @@
 var routesBaseUrl = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc"
 var directionsBaseUrl = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r="
 var stopBaseUrl = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId="
-// var ttcNextBus = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=32"
-// var whatsComing = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId="
-// var routeTag = "&routeTag=32"
-var json = {};
-// function not being called yet
-// function get_stop(stop_id) {
-//   return "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=" + stop_id + "&routeTag=32"
-// };
 
 
-// this url gets specific for the stop id and the routeTag
-// "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=<agency_tag>&stopId=<stop id>&routeTag=<route tag>"
 $(document).ready(function(){
+var routes,
+    currentRouteTag,
+    directions,
+    routeStops;
 
   // Get routes
   $.get(routesBaseUrl, function(xml){
-    json = $.xml2json(xml);
-    var routes = json.route
+    var response = $.xml2json(xml);
+    routes = response.route
     routes.forEach(function(route, index){
       $('#myRoute').append("<option value='" + route.tag + "'>" + route.title + "</option>");
       $('#myRoute').prop("selectedIndex", -1);
@@ -29,10 +23,12 @@ $(document).ready(function(){
   $('#myRoute').on('change', function(){
     $('#myDirection').html("");
     $('#myStop').html("");
+    currentRouteTag = this.value;
     var directionsUrl = (directionsBaseUrl + this.value);
     $.get(directionsUrl, function(xml){
-      json = $.xml2json(xml);
-      var directions = json.route.direction
+      var response = $.xml2json(xml);
+      directions = response.route.direction;
+      routeStops = response.route.stop;
       directions.forEach(function(direction, index){
         $('#myDirection').append("<option value='" + direction.tag + "'>" + direction.title + "</option>");
         $('#myDirection').prop("selectedIndex", -1);
@@ -45,9 +41,10 @@ $(document).ready(function(){
     $('#myStop').html("");
     var stops = [];
     var menuValue = this.value;
-    json.route.direction.forEach(function(direction) {
+    if (!directions) return;
+    directions.forEach(function(direction) {
       if (direction.tag == menuValue) {
-        direction.stop.forEach(function(stop) {
+        routeStops.forEach(function(stop) {
           stops.push(stop);
         });
       } 
@@ -57,36 +54,37 @@ $(document).ready(function(){
       $('#myStop').append("<option value='" + stop.tag + "'>" + stop.tag + "</option>");
       $('#myStop').prop("selectedIndex", -1);
     });
-
-    $("#myStop").on("change", function(){
-      $("#upcoming").html("");
-      console.log(this.value);
-      var stopMenuValue = this.value;
-      json.route.stop.forEach(function(stopTag){
-        if (stopTag.tag == stopMenuValue) {
-          var stopIdUrl = (stopBaseUrl + (stopTag.stopId) + "&routeTag=" + (json.route.tag));
-          $.get(stopIdUrl, function(xml){
-            json = $.xml2json(xml);
-            if (typeof json.predictions.direction === "undefined") {
-              $("#yourTimes").html("Nothing's coming..now. Please check back soon!!");
-            } 
-            var estimateTime = json.predictions.direction.prediction;
-            estimateTime.forEach(function(coming){
-              var predTime = parseInt(coming.seconds);
-              var betterTime = moment.duration(predTime, 'seconds');
-              var hours = Math.floor(betterTime.asHours());
-              var mins = Math.floor(betterTime.asMinutes()) - hours * 60;
-              $("#upcoming").append('<li>' + ("h: " + hours + " m: " + mins) + '</li>');
-              $("#yourTimes").html('Your upcoming times');
-            });
-          })
-        };
-      });
-
+  });
+  $("#myStop").on("change", function(){
+    $("#upcoming").html("");
+    var stopMenuValue = this.value;
+    routeStops.forEach(function(stopTag){
+      if (stopTag.tag == stopMenuValue) {
+        var stopIdUrl = (stopBaseUrl + stopTag.stopId + "&routeTag=" + currentRouteTag);
+        $.get(stopIdUrl, function(xml){
+          var response = $.xml2json(xml);
+          if (!response.predictions || typeof response.predictions.direction === "undefined") {
+            $("#yourTimes").html("Nothing's coming..now. Please check back soon!!");
+            return;
+          } 
+          var estimateTime = response.predictions.direction.prediction;
+          debugger
+          estimateTime.forEach(function(coming){
+            var predTime = parseInt(coming.seconds);
+            var betterTime = moment.duration(predTime, 'seconds');
+            var hours = Math.floor(betterTime.asHours());
+            var mins = Math.floor(betterTime.asMinutes()) - hours * 60;
+            $("#upcoming").append('<li>' + ("h: " + hours + " m: " + mins) + '</li>');
+            $("#yourTimes").html('Your upcoming times');
+          });
+        })
+      };
     });
 
+  });
+
 return true;
-})
+
 });
 
 // TODO   
